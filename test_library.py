d0441574c99b20c087d2da963cb30c4de2008cd0
Darlene250 <d.ayinkamiy@alustudent.com>
2025-07-27 13:50:49 +0200

    Update test_library.py

--- test_library.py
+++ test_library.py
@@ -5,3 +5,17 @@
 import unittest  # This is Python's built-in testing tool
 from unittest.mock import patch  # This helps us "pretend" user input and function results
 import admin  # We're testing functions from the admin.py file
+
+# Testing Add New Book
+
+class TestAdminAddBook(unittest.TestCase):
+    # We use @patch to mock input() and the load/save functions
+    @patch('admin.load_books')  # Pretend the books list is coming from here
+    @patch('admin.save_books')  # Pretend we’re saving the books somewhere
+    @patch('builtins.input', side_effect=[
+        'Python Basics',  # title
+        'Alice Walker',   # author
+        'Computing',      # subject
+        'English',        # language
+        '/books/python.pdf'  # file path
+    ])
